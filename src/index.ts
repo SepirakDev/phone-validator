fa1fcc3b96dd03ecae4955c63a3756135531e6d3
Nick Harris <nick@sepirak.com>
2025-01-21 09:22:35 -0800

    chore: prettier

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
-import {Hono} from 'hono';
-import {vValidator} from '@hono/valibot-validator';
-import {validPhoneNumber} from "./vali-schema.ts";
+import { Hono } from 'hono';
+import { vValidator } from '@hono/valibot-validator';
+import { validPhoneNumber } from './vali-schema.ts';
 import { object } from 'valibot';
 
 const app = new Hono();
 
-app.get('/health', (c) => c.json({health: 'OK'}));
+app.get('/health', (c) => c.json({ health: 'OK' }));
 
-app.post('/validate', vValidator('json', object({
-  phoneNumber: validPhoneNumber(),
-})), (c) => {
-  const data = c.req.valid('json');
+app.post(
+  '/validate',
+  vValidator(
+    'json',
+    object({
+      phoneNumber: validPhoneNumber(),
+    }),
+  ),
+  (c) => {
+    const data = c.req.valid('json');
 
-  return c.json({
-    phoneNumber: data.phoneNumber,
-  });
-});
+    return c.json({
+      phoneNumber: data.phoneNumber,
+    });
+  },
+);
 
 export default app;
